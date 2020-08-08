cc772f8d4c6a5b286721498fb3fd5e29ae3cf11d
gladkill <gladkillmusic@gmail.com>
2020-08-07 22:25:56 -0700

    split div card into own component, finished styling.

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,9 +2,7 @@ import React, { useContext } from 'react';
 import TokenContext from "./TokenContext";
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import HomepageCard from './HomepageCard';
 
 
 
@@ -14,10 +12,18 @@ function HomePage() {
 
   if (userName !== null) {
     return (
-      <div>
+      <div className="app">
+        <div className="mt-5 px-2">
         <h1 className="primary-font"> Welcome Back!</h1>
-        <h3> Start your job search now.</h3>
-        <Link to="/companies"><Button variant="dark">Search</Button></Link>
+        <h3 className="text-white secondary-font"> Start your job search now.</h3>
+        </div>
+        <div className="mt-5">
+        <h4 className="primary-font">Keep looking - your dream job awaits.</h4>
+        <HomepageCard />
+        <Link to="/companies">
+          <Button className="mt-4" variant="dark">Companies</Button>
+          </Link>
+      </div>
       </div>
     )
 
@@ -28,38 +34,7 @@ function HomePage() {
           <h1 className="primary-font">Welcome to Career Cat!</h1>
           <h2 className="text-white secondary-font">Find Your Purpose. Find Your Culture.</h2>
         </div>
-        <div className="w-50  mt-5 m-auto pt-5 px-2">
-          <Container>
-          <Row>
-            <Col xs="12" lg="4">
-              <div className="mx-1">
-              <i className=" fa-10x fas fa-cat"></i>
-                <p className="mt-3 card-font">
-                  Career Cat provides the most relevant, cutting-edge jobs for our users.
-                </p>
-              </div>
-            </Col>
-            <Col xs="12" lg="4">
-              <div className="mx-1">
-              <i className=" fa-10x fas fa-laptop"></i>
-                <p className="mt-3 card-font">
-                  We link companies with the perfect employee, from skills, to culture, to work life balance.
-                </p>
-              </div>
-            </Col>
-            <Col xs="12" lg="4">
-              <div className="mx-1">
-                <i className=" fa-10x fas fa-users"></i>
-                <p className="mt-3 card-font">
-                  We don't just help you find a new job-we help you find your new work family.
-                </p>
-              </div>
-            </Col>
-
-          </Row>
-          </Container>
-        </div>
-
+        <HomepageCard />
         <div className="mt-5">
         <h4 className="primary-font">Log in or sign up to start your search today.</h4>
         <Link to="/login">
---
+++ src/HomepageCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import './HomepageCard.css';
+
+
+function HomepageCard(){
+  return (
+    <div className="w-50 mt-5 m-auto pt-5 px-2">
+    <Container>
+    <Row>
+      <Col xs="12" lg="4">
+        <div className="mx-1 cardie">
+        <i className=" fa-10x fas fa-cat"></i>
+          <p className="mt-3 card-font">
+            Career Cat provides the most relevant, cutting-edge jobs for our users.
+          </p>
+        </div>
+      </Col>
+      <Col xs="12" lg="4">
+        <div className="mx-1 cardie">
+        <i className=" fa-10x fas fa-laptop"></i>
+          <p className="mt-3 card-font">
+            We link companies with the perfect employee, from skills, to culture, to work life balance.
+          </p>
+        </div>
+      </Col>
+      <Col xs="12" lg="4">
+        <div className="mx-1 cardie">
+          <i className=" fa-10x fas fa-users"></i>
+          <p className="mt-3 card-font">
+            We don't just help you find a new job-we help you find your new work family.
+          </p>
+        </div>
+      </Col>
+
+    </Row>
+    </Container>
+  </div>
+
+  )
+
+}
+
+export default HomepageCard
